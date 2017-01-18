dd7f5fa2cc1e63d2f7cc24141173577f45d81841
John Krueger <john.krueger@nrel.gov>
2017-01-17 20:28:28 -0700

    add more controller tests & get rid of that pesky test warning

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,9 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  context '#index' do
-    it 'gets all the posts'
+  before(:all) do
+    @user = build(:user)
+  end
 
-    it 'renders the index view'
+  describe '#index' do
+    it 'gets all the posts' do
+      post = create(:post, :user_id => @user.id)
+
+      get :index
+      assigns(:posts).should eq([post])
+
+      post.destroy
+    end
+
+    it 'should return a 200 HTTP code' do
+      response.status.should eq(200)
+    end
+
+    it 'renders the index view' do
+      get :index
+      response.should render_template :index
+    end
   end
 end
